0f8e534980aa7d4cc355427f6ceea4a3991d01dd
Yohannes Accrus <yohanesaccrus014@gmail.com>
2021-11-21 21:08:17 +0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+
+const HomePage = () => {
+
+	useEffect(() => {
+		alert('adsadas')
+	}, [])
+
+	return (
+		<div>Welcome to Next.js!</div>
+	)
+}
+
+export default HomePage
